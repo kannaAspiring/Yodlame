9d25bf552fcb569f8340db4ac04e5ee24a1914ea
Aumi <oumi-4@hotmail.com>
2011-08-17 12:38:14 +0700

    Edit

--- PuzzleByyodlame/PuzzleByyodlame/src/puzzlebyyodlame/PuzzleByyodlame.java
+++ PuzzleByyodlame/PuzzleByyodlame/src/puzzlebyyodlame/PuzzleByyodlame.java
@@ -2,9 +2,17 @@
 package puzzlebyyodlame;
 import java.awt.Image;
 import java.awt.Toolkit;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
+
 public class PuzzleByyodlame extends JFrame{
+    //  private Image source;
+    //  private Image image;
+      int width, height;
+       
      
     public PuzzleByyodlame(String title){
          this();
@@ -12,18 +20,27 @@ public class PuzzleByyodlame extends JFrame{
      }
    
     public PuzzleByyodlame(){
+            
+  
+        Toolkit kit = Toolkit.getDefaultToolkit();
+    
+             
         
-       setSize(400,300);//มีการกำหนดขนาด เพราะว่า frame ที่สร้างขึ้น default มันจะมีขนาดเป็นศุนย์ และไม่สามารถมองเห็นได้
-       setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
-        Toolkit kit =Toolkit.getDefaultToolkit();
-        Image img = kit.getImage("path file image");
-       setIconImage(img);
+        setSize(400,300);//มีการกำหนดขนาด เพราะว่า frame ที่สร้างขึ้น default มันจะมีขนาดเป็นศุนย์ และไม่สามารถมองเห็นได้
+        setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
+        Image img = kit.getImage("Image/puzzle_blue.png");
+        setIconImage(img);
+       
+        Image img1 = kit.getImage("Image/social.jpg");
+        Icon icon1 = new ImageIcon(img1);     
+        JButton b1 =new JButton(icon1);
+        add(b1);
+        
+    
+       
+     
+                 
        
     }
 
-    public static void main(String[] args) {
-        PuzzleByyodlame frame = new PuzzleByyodlame("Puzzle Funny");
-        frame.setVisible(true);
-        
-    }
 }
---
+++ PuzzleByyodlame/PuzzleByyodlame/src/puzzlebyyodlame/TestPuzzle.java
@@ -0,0 +1,16 @@
+package puzzlebyyodlame;
+
+
+    public abstract class TestPuzzle {
+        
+        public static void main(String[] args) {
+           
+               new PuzzleByyodlame();
+               PuzzleByyodlame frame = new PuzzleByyodlame("Puzzle Funny");
+               frame.setVisible(true);
+        }
+
+    }
+
+
+
