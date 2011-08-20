7b8284c48b9d7abbb511e1c1397b8a4eaf71e14b
Aumi <oumi-4@hotmail.com>
2011-08-21 01:40:34 +0700

    Code  Modify

--- PuzzleByyodlame/src/puzzlebyyodlame/PuzzleByyodlame.java
+++ PuzzleByyodlame/src/puzzlebyyodlame/PuzzleByyodlame.java
@@ -1,4 +1,3 @@
-
 package puzzlebyyodlame;
 
 import java.awt.*;
@@ -12,22 +11,19 @@ import javax.swing.*;
 
 
 public class PuzzleByyodlame extends  JFrame implements ActionListener{
-     private JMenuBar menuBar; 
-     private JMenu fileMenu; 
-     private JMenuItem easy; 
-     private JMenuItem normal;
-     private JMenuItem hard;
-     private JMenuItem exitMenu;    
-     private JLabel display;
+
+    private JLabel lblstatus;
     private Image source;
     private Image image;
     private Image ranImage;
     private JLabel label;
-    private JPanel centerPanel;
-    private JButton button;
+    private JPanel centerPanel,panel;
+    private JButton button,b1,b2,b3;
     private byte[] imgByte;
     int[][] position;
-    int index,level;
+    int index;
+    int level = 1;
+    int zero = 0;
     int width, height;
     int x,y,z;
     int temp;
@@ -43,41 +39,48 @@ public class PuzzleByyodlame extends  JFrame implements ActionListener{
    }
    
     public PuzzleByyodlame(){
-           menuBar = new JMenuBar(); 
-     
-           fileMenu = new JMenu("New Game"); 
-           fileMenu.setMnemonic(KeyEvent.VK_F); 
-         
-           easy = new JMenuItem("Easy", KeyEvent.VK_E); 
-           normal = new JMenuItem("Normal",KeyEvent.VK_N);
-           hard = new JMenuItem("Hard",KeyEvent.VK_H);
-           exitMenu = new JMenuItem("Exit",KeyEvent.VK_X); 
-     
-           easy.addActionListener(this); 
-           normal.addActionListener(this);
-           hard.addActionListener(this);
-           exitMenu.addActionListener(this);
-           
-           menuBar.add(fileMenu);            
-           fileMenu.add(easy);
-           fileMenu.add(normal);
-           fileMenu.add(hard);
-           fileMenu.add(exitMenu);
-           
-           setJMenuBar(menuBar);        
-                                
-            position = new int[][] {
+       switch(level){
+            case 1 :position = new int[][] { //3*3
                             {0, 1, 2}, 
                             {3, 4, 5}, 
                             {6, 7, 8}
-                                   };
+                      };
              index = 3;
+                break;
+            case 2 :position = new int[][] { //4*4
+                            {0, 1, 2, 3}, 
+                            {4, 5, 6, 7}, 
+                            {8, 9, 10, 11},
+                            {12, 13, 14, 15}
+                        };
+            index = 4;
+                break;
+}  
+        b1 =new JButton("New Game");
+        b2 =new JButton("Help me");
+        b3 =new JButton("Reset");
+        
+        
+        lblstatus = new JLabel("Start Game(10000)"); 
+        
+        panel =new JPanel();
+        panel.add(b1);
+        panel.add(b2);
+        panel.add(b3);
+        
+        b1.addActionListener(this);
+        b2.addActionListener(this);
+        b3.addActionListener(this);
+        
+          
              initComponents();
-             System.out.println(index);
+             //System.out.println(index);
            
        
-        add(Box.createRigidArea(new Dimension(0, 5)), BorderLayout.NORTH);    
+        //add(Box.createRigidArea(new Dimension(0, 5)), BorderLayout.NORTH);    
         add(centerPanel, BorderLayout.CENTER);
+        add(lblstatus,BorderLayout.SOUTH); 
+        add(panel, BorderLayout.NORTH);
        
         for ( int i = 0; i < index; i++) {
             for ( int j = 0; j < index; j++) {
@@ -88,22 +91,20 @@ public class PuzzleByyodlame extends  JFrame implements ActionListener{
                     lineOfImg.add(image);
                 } 
             }
-        Check = lineOfImg;
-        System.out.println(Check.get(0));
+        Check = lineOfImg;       
         temp = lineOfImg.size(); 
-        //lineOfImg.add(null); 
-        //System.out.println(lineOfImg.size());
+
         z = generator.nextInt(lineOfImg.size());
         y = lineOfImg.size();
         
        
         RandomPic();
        
-        setSize(width,height);
+        setSize(800,600);
         setResizable(false);
         setLocationRelativeTo(null);
         setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
-        
+        setVisible(true);
     }
     
     
@@ -158,7 +159,7 @@ public class PuzzleByyodlame extends  JFrame implements ActionListener{
                     width = sid.getIconWidth();
                     height = sid.getIconHeight();
                 break;
-            case 1: ImageIcon sid2 = new ImageIcon(kit.getImage("Image/10.jpg"));
+            case 1: ImageIcon sid2 = new ImageIcon(kit.getImage("Image/2.jpg"));
                     source = sid2.getImage();
                     width = sid2.getIconWidth();
                     height = sid2.getIconHeight();
@@ -198,7 +199,7 @@ public class PuzzleByyodlame extends  JFrame implements ActionListener{
                     width = sid9.getIconWidth();
                     height = sid9.getIconHeight();
                 break;
-            case 9: ImageIcon sid10 = new ImageIcon(kit.getImage("Image/0.jpg"));
+            case 9: ImageIcon sid10 = new ImageIcon(kit.getImage("Image/10.jpg"));
                     source = sid10.getImage();
                     width = sid10.getIconWidth();
                     height = sid10.getIconHeight();
@@ -215,18 +216,26 @@ public class PuzzleByyodlame extends  JFrame implements ActionListener{
      public void actionPerformed(ActionEvent e) {
          
         Object source = e.getSource();
-        if(source ==easy)
-             System.out.println("Easy");
-        else if(source==normal)
-            System.out.println("Normal");
-        else if(source==hard)
-            System.out.println("Hard");
-        else if(source==exitMenu){
-        int val = JOptionPane.showConfirmDialog(this, "Do you want to exit programe?", "Exit", JOptionPane.OK_CANCEL_OPTION);
-        if(val== JOptionPane.OK_OPTION)
+        
+        if(source == b1){
+        int val = JOptionPane.showConfirmDialog(this, "Do you want to new game?", "New Game", JOptionPane.OK_CANCEL_OPTION);
+            if(val== JOptionPane.OK_OPTION){
+            level = index - 2;
+            JOptionPane.showMessageDialog(this, "Your Pic is Changed");
+            new PuzzleByyodlame("Puzzle Funny");
+            }
+        }
+        else if(source==b2){
+            JOptionPane.showMessageDialog(this, "Help Yourself ^ ^");
+            
+        }
+        else if(source==b3){
+            int val2 = JOptionPane.showConfirmDialog(this, "Are you sure?", "Reset", JOptionPane.OK_CANCEL_OPTION);
+        if(val2== JOptionPane.OK_OPTION){}
+         JOptionPane.showMessageDialog(this, "Your Score is :  " + count*50);
         System.exit(0);
-}
-         
+        }
+  
         JButton button = (JButton) e.getSource();
         Dimension size = button.getSize();
 
@@ -249,7 +258,7 @@ public class PuzzleByyodlame extends  JFrame implements ActionListener{
              centerPanel.add(button,labelIndex);
              centerPanel.validate();
              count++;
-             System.out.println("Count :  " + count);
+             lblstatus.setText("Count :  " + count);
         }
 
         if (labelX == buttonX && (labelY - buttonY) == -size.height ) {
@@ -260,7 +269,7 @@ public class PuzzleByyodlame extends  JFrame implements ActionListener{
              centerPanel.add(label, buttonIndex);
              centerPanel.validate();
              count++;
-             System.out.println("Count :  " + count);
+             lblstatus.setText("Count :  " + count);
         }
 
         if (labelY == buttonY && (labelX - buttonX) == size.width ) {
@@ -272,7 +281,7 @@ public class PuzzleByyodlame extends  JFrame implements ActionListener{
              centerPanel.add(button,labelIndex);
              centerPanel.validate();
              count++;
-             System.out.println("Count :  " + count);
+             lblstatus.setText("Count :  " + count);
         }
 
         if (labelY == buttonY && (labelX - buttonX) == -size.width ) {
@@ -284,9 +293,23 @@ public class PuzzleByyodlame extends  JFrame implements ActionListener{
              centerPanel.add(button,labelIndex);
              centerPanel.validate();
              count++;
-             System.out.println("Count :  " + count);
+             lblstatus.setText("Count :  " + count);
         }
         
+           switch(index-2){
+                case 1: if(count == 150){ //3*3
+                    System.out.println("Game over");
+                     JOptionPane.showMessageDialog(this, "Your Score is :  " + zero);
+                    System.exit(0);
+                    }
+                break;   
+                case 2: if(count == 750){ //4*4
+                    System.out.println("Game over");
+                     JOptionPane.showMessageDialog(this, "Your Score is :  " + zero);
+                     System.exit(0);
+                    }
+                break;
+        }
 
     }
 }
--- PuzzleByyodlame/src/puzzlebyyodlame/TestPuzzle.java
+++ PuzzleByyodlame/src/puzzlebyyodlame/TestPuzzle.java
@@ -4,7 +4,7 @@ package puzzlebyyodlame;
     public class TestPuzzle {
         
         public static void main(String[] args) {
-        PuzzleByyodlame frame = new PuzzleByyodlame();                 
+        PuzzleByyodlame frame = new PuzzleByyodlame("Puzzle Funny");                 
         frame.setVisible(true);
         
         }
