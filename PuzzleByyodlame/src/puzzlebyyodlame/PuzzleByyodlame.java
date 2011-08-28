0853a48943e11edb689635742522e5a65af611f4
Thanapol Yaprom <Blade_children_27@hotmail.com>
2011-08-28 15:46:36 +0700

    Fin

--- PuzzleByyodlame/src/puzzlebyyodlame/PuzzleByyodlame.java
+++ PuzzleByyodlame/src/puzzlebyyodlame/PuzzleByyodlame.java
@@ -8,8 +8,6 @@ import java.awt.image.*;
 import java.util.*;
 import javax.swing.*;
 
-
-
 public class PuzzleByyodlame extends  JFrame implements ActionListener{
 
     private JLabel lblstatus;
@@ -17,8 +15,8 @@ public class PuzzleByyodlame extends  JFrame implements ActionListener{
     private Image image;
     private Image ranImage;
     private JLabel label;
-    private JPanel centerPanel,panel;
-    private JButton button,b1,b2,b3;
+    private JPanel centerPanel,panel,lvpanel;
+    private JButton button,b1,b2,b3,btn1,btn2,btn3,btn4;
     private byte[] imgByte;
     int[][] position;
     int index;
@@ -28,12 +26,13 @@ public class PuzzleByyodlame extends  JFrame implements ActionListener{
     int x,y,z;
     int temp;
     int breaking,count = 0;
+    int selectlv;
   
     Random generator = new Random();
     ArrayList lineOfImg = new ArrayList();
     ArrayList Check = new ArrayList();
      
-         public PuzzleByyodlame(String title){
+    public PuzzleByyodlame(String title){
             this();
             setTitle(title);
    }
@@ -72,16 +71,34 @@ public class PuzzleByyodlame extends  JFrame implements ActionListener{
         b2.addActionListener(this);
         b3.addActionListener(this);
         
-          
+        btn1 =new JButton("Easy"); //300
+        btn2 =new JButton("Normal"); //150
+        btn3 =new JButton("Hard"); //75
+        btn4 =new JButton("Fin"); //75
+        
+        lvpanel = new JPanel();
+        lvpanel.setLayout(new GridLayout(4, 1, 0, 0));
+        lvpanel.add(btn1);
+        lvpanel.add(btn2);
+        lvpanel.add(btn3);
+        lvpanel.add(btn4);
+        
+        btn1.addActionListener(this);
+        btn2.addActionListener(this);
+        btn3.addActionListener(this);
+        btn4.addActionListener(this);
+
              initComponents();
              //System.out.println(index);
            
        
         //add(Box.createRigidArea(new Dimension(0, 5)), BorderLayout.NORTH);    
         add(centerPanel, BorderLayout.CENTER);
+        add(lvpanel, BorderLayout.EAST);
         add(lblstatus,BorderLayout.SOUTH); 
         add(panel, BorderLayout.NORTH);
        
+        
         for ( int i = 0; i < index; i++) {
             for ( int j = 0; j < index; j++) {
   
@@ -91,6 +108,7 @@ public class PuzzleByyodlame extends  JFrame implements ActionListener{
                     lineOfImg.add(image);
                 } 
             }
+        
         Check = lineOfImg;       
         temp = lineOfImg.size(); 
 
@@ -99,14 +117,15 @@ public class PuzzleByyodlame extends  JFrame implements ActionListener{
         
        
         RandomPic();
-       
+        JOptionPane.showMessageDialog(this, "Please select level for play");
+        JOptionPane.showMessageDialog(this, "My default level is Normal");
+        JOptionPane.showMessageDialog(this, "Your move is 150 moves");
         setSize(800,600);
         setResizable(false);
         setLocationRelativeTo(null);
         setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
         setVisible(true);
     }
-    
     
     public void RandomPic(){
          
@@ -210,32 +229,85 @@ public class PuzzleByyodlame extends  JFrame implements ActionListener{
        
     
     }
-        
-    
-    
-     public void actionPerformed(ActionEvent e) {
+          
+    public void actionPerformed(ActionEvent e) {
          
         Object source = e.getSource();
+        
+        selectlv = 2;
         
         if(source == b1){
         int val = JOptionPane.showConfirmDialog(this, "Do you want to new game?", "New Game", JOptionPane.OK_CANCEL_OPTION);
             if(val== JOptionPane.OK_OPTION){
             level = index - 2;
-            JOptionPane.showMessageDialog(this, "Your Pic is Changed");
-            new PuzzleByyodlame("Puzzle Funny");
+            JOptionPane.showMessageDialog(this, "Your Pic is Changed.");
+            new PuzzleByyodlame("Puzzle Funny.");
             }
         }
-        else if(source==b2){
-            JOptionPane.showMessageDialog(this, "Help Yourself ^ ^");
+        
+        else if(source==b2){      
+            JOptionPane.showMessageDialog(this, "Help Yourself ^ ^.");
             
         }
+        
         else if(source==b3){
             int val2 = JOptionPane.showConfirmDialog(this, "Are you sure?", "Reset", JOptionPane.OK_CANCEL_OPTION);
-        if(val2== JOptionPane.OK_OPTION){}
+        if(val2== JOptionPane.OK_OPTION){
          JOptionPane.showMessageDialog(this, "Your Score is :  " + count*50);
         System.exit(0);
         }
-  
+        }
+        
+        else if(source==btn1){
+        int val2 = JOptionPane.showConfirmDialog(this, "Are you sure?", "Easy", JOptionPane.OK_CANCEL_OPTION);
+        if(val2== JOptionPane.OK_OPTION){
+        JOptionPane.showMessageDialog(this,"This is Easy Mode.");
+        JOptionPane.showMessageDialog(this,"Your move is 300 moves.");
+        selectlv = 1;
+        }
+        }
+        
+        else if(source==btn2){
+        int val2 = JOptionPane.showConfirmDialog(this, "Are you sure?", "Normal", JOptionPane.OK_CANCEL_OPTION);
+        if(val2== JOptionPane.OK_OPTION){
+        JOptionPane.showMessageDialog(this,"This is Normal Mode.");
+         JOptionPane.showMessageDialog(this,"Your move is 150 moves.");
+        selectlv = 2;
+        }
+        }
+        
+        else if(source==btn3){
+        int val2 = JOptionPane.showConfirmDialog(this, "Are you sure?", "Hard", JOptionPane.OK_CANCEL_OPTION);
+        if(val2== JOptionPane.OK_OPTION){
+        JOptionPane.showMessageDialog(this,"This is Hard Mode.");
+        JOptionPane.showMessageDialog(this,"Your move is 75 moves.");
+        selectlv = 3;
+        }
+        }
+       
+        else if(source==btn4){
+        JOptionPane.showMessageDialog(this,"จบเกมส์จริงหรอ");
+        JOptionPane.showMessageDialog(this,"ตรงตำแหน่งหรือยัง");
+        JOptionPane.showMessageDialog(this,"ซื่อสัตย์ต่อตนเองนะครับ");
+        JOptionPane.showMessageDialog(this,"ห้ามโกงหล่ะ");
+        int val2 = JOptionPane.showConfirmDialog(this, "Are you sure?", "Finish", JOptionPane.OK_CANCEL_OPTION);
+        if(val2== JOptionPane.OK_OPTION){
+        JOptionPane.showMessageDialog(this, "Your Score is :  " + (10000 - (count*50)));
+        JOptionPane.showMessageDialog(this,"Congratulation!!");
+        int val3 = JOptionPane.showConfirmDialog(this, "Are you want to play again?", "Finish", JOptionPane.OK_CANCEL_OPTION);
+            if (val3 == JOptionPane.OK_OPTION) {
+                level = index - 2;
+            JOptionPane.showMessageDialog(this, "As you wish.");
+            new PuzzleByyodlame("Puzzle Funny");
+            }
+            else{
+            JOptionPane.showMessageDialog(this,"Thank you for playing.");
+            JOptionPane.showMessageDialog(this,"See Ya.");
+            System.exit(0);
+            }
+        }
+        }
+        
         JButton button = (JButton) e.getSource();
         Dimension size = button.getSize();
 
@@ -246,8 +318,8 @@ public class PuzzleByyodlame extends  JFrame implements ActionListener{
         int buttonPosX = buttonX / size.width;
         int buttonPosY = buttonY / size.height;
         int buttonIndex = position[buttonPosY][buttonPosX];
-
-
+        Check.add(button);
+          //System.out.println(buttonIndex);
 
         if (labelX == buttonX && (labelY - buttonY) == size.height ) {
 
@@ -296,15 +368,21 @@ public class PuzzleByyodlame extends  JFrame implements ActionListener{
              lblstatus.setText("Count :  " + count);
         }
         
-           switch(index-2){
-                case 1: if(count == 150){ //3*3
-                    System.out.println("Game over");
+           switch(selectlv){
+                case 1: if(count == 300){ 
+                    JOptionPane.showMessageDialog(this,"Game over.");
                      JOptionPane.showMessageDialog(this, "Your Score is :  " + zero);
                     System.exit(0);
                     }
                 break;   
-                case 2: if(count == 750){ //4*4
-                    System.out.println("Game over");
+                case 2: if(count == 150){ 
+                    JOptionPane.showMessageDialog(this,"Game over.");
+                     JOptionPane.showMessageDialog(this, "Your Score is :  " + zero);
+                     System.exit(0);
+                    }
+                break;
+                      case 3: if(count == 75){ 
+                    JOptionPane.showMessageDialog(this,"Game over");
                      JOptionPane.showMessageDialog(this, "Your Score is :  " + zero);
                      System.exit(0);
                     }
